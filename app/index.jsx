c7c91cd0850e15bccca9c43b31a0e16e1f660c41
Zyuzin Vitaly <juwain@yandex.ru>
2015-10-01 01:18:15 +0300

    return additional wrapper for app

--- app/index.jsx
+++ app/index.jsx
@@ -4,5 +4,11 @@ import App from './components/App/App.jsx';
 main();
 
 function main() {
-  React.render(<App />, document.body);
+  const app = document.createElement('div');
+
+  app.className = 'app';
+
+  document.body.appendChild(app);
+
+  React.render(<App />, app);
 }
